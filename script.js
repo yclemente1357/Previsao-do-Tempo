5e78ba8130e7bc2f7a6450beb8938f4641cec8d8
Yuri Alves Clemente <116123245+yclemente1357@users.noreply.github.com>
2024-03-19 09:59:35 -0300

    Incluindo Favicon do projeto

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function colocarDadosNaTela(dados) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".umidade").innersHTML = "Umidade: " + dados.main.humidity + "%"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 
 }
