a179a2ab0a2ab71674ba6875c152d3e01a4aa11b
yuri-alves <yclemente2003@gmail.com>
2024-03-18 22:28:51 -0300

    Criando Projeto

---
+++ script.js
@@ -0,0 +1,24 @@
+const key = "fcfe94078935b8b113eb6b888aab5794"
+
+function colocarDadosNaTela(dados) {
+
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+// async, função asincronas.innerHTML = dados.n
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    // await, próprio para aguardar uma resposta do servidor e fetch, para acessar o servidor, then utilizado para retornar uma resposta, formatando ela em um arquivo json
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
